033056261bbb16f33d8e93b0d6d0ba745a80226b
Chris Palka <cpalka87@gmail.com>
2019-04-06 19:25:02 -0700

    Implement devise gem and initial configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios
 
   get 'about-me', to: 'portfolios#about'
   get 'contact', to: 'portfolios#contact'
+
+  root to: 'portfolios#index'
 end
