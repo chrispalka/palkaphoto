b32b219e28fcf027237b20be4b6323b8346d91dc
Chris Palka <cpalka87@gmail.com>
2019-04-08 18:31:28 -0700

    Implement petergate for authorization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,43 @@
 module ApplicationHelper
+
+  def login_helper
+		if current_user.is_a?(GuestUser)
+      (link_to "Register", new_user_registration_path) +
+      " ".html_safe +
+      (link_to "Login", new_user_session_path)
+    else
+    	link_to "Logout", destroy_user_session_path, method: :delete
+    end
+	end
+
+#   def nav_items
+#   [
+#     {
+#       url: root_path,
+#       title: 'Home'
+#     },
+#     {
+#       url: about_me_path,
+#       title: 'About Me'
+#     },
+#     {
+#       url: contact_path,
+#       title: 'Contact'
+#     }
+#   ]
+# end
+#
+# def nav_helper style, tag_type
+#   nav_links = ''
+#
+#   nav_items.each do |item|
+#     nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+#   end
+#
+#   nav_links.html_safe
+# end
+
+
   def copyright_generator
      PalkaphotoViewTool::Renderer.copyright 'Chris Palka', 'All rights reserved'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios
 
+  # get '/:id/edit', to: 'portfolios#edit', as: 'edit_portfolio_path'
+  get 'new', to: 'portfolios#new'
   get 'about-me', to: 'portfolios#about'
   get 'contact', to: 'portfolios#contact'
-  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get '/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   root to: 'portfolios#index'
 end
