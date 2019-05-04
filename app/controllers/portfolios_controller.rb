fdd8ae2890252dc321cce543d9bc69d2214cb500
Chris Palka <cpalka87@gmail.com>
2019-05-04 02:18:40 -0700

    Add delete functionality if logged in as site admin

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -74,7 +74,7 @@ class PortfoliosController < ApplicationController
   def destroy
     @portfolio.destroy
     respond_to do |format|
-      format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'Portfolio was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
