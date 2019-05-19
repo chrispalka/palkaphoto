b1521e7131f92d35286382c4134467a7ed381e08
Chris Palka <cpalka87@gmail.com>
2019-05-18 18:47:01 -0700

    Initial implementation of nav links and helper methods associated with

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,29 @@
 module ApplicationHelper
 
-  def login_helper style = ''
-		if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path, class: style) +
-      "<br>".html_safe +
-      (link_to "Login", new_user_session_path, class: style)
+  def login_helper style = '', page
+    if page == 'Portfolio'
+  		if current_user.is_a?(GuestUser)
+        (link_to "Register", new_user_registration_path, class: style) +
+        "<br>".html_safe +
+        (link_to "Login", new_user_session_path, class: style)
+      else
+      	(link_to "Logout", destroy_user_session_path, method: :delete, class: style) +
+        "<br>".html_safe +
+        (link_to "My Account", edit_user_registration_path, class: style) +
+        "<br>".html_safe +
+        (link_to "New", new_path, class: style)
+      end
     else
-    	(link_to "Logout", destroy_user_session_path, method: :delete, class: style) +
-      "<br>".html_safe +
-      (link_to "My Account", edit_user_registration_path, class: style) +
-      "<br>".html_safe +
-      (link_to "New", new_path, class: style)
-
+      if current_user.is_a?(GuestUser)
+        (link_to "Register", new_user_registration_path, class: style) +
+        (link_to "Login", new_user_session_path, class: style)
+      else
+      	(link_to "Logout", destroy_user_session_path, method: :delete, class: style)
+        (link_to "My Account", edit_user_registration_path, class: style)
+        (link_to "New", palkafoodie_new_path, class: style)
+      end
     end
-	end
+  end
 
   def nav_items
   [
