eb6675d902f15f180868e1a3895e0b228debf0d2
Chris Palka <cpalka87@gmail.com>
2019-05-25 00:42:17 -0700

    Updated seeds file to include categories and blog association to categories

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,10 +1,11 @@
 class Blog < ApplicationRecord
-  belongs_to :category
 
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  validates_presence_of :title, :body
+  validates_presence_of :title, :body, :category_id
+
+  belongs_to :category, optional: true
 
   def self.recent_blogs
   		order('created_at desc')
