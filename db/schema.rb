30ab9c496a3edfd1b8095e27bd256f1ee8301697
Chris Palka <cpalka87@gmail.com>
2019-05-29 17:13:39 -0700

    Clean more migration files before production

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_220939) do
+ActiveRecord::Schema.define(version: 2019_05_30_001223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,9 @@ ActiveRecord::Schema.define(version: 2019_05_29_220939) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.bigint "category_id"
-    t.string "subtitle"
     t.integer "status", default: 0
     t.text "blog_image"
+    t.string "subtitle"
     t.index ["category_id"], name: "index_blogs_on_category_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
